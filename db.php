c5f0e418ff0e98554147a6345d2c10287419925c
idkhowto <kamdemcreator@gmail.com>
2024-08-05 17:05:42 -0500

    Create db.php

---
+++ db.php
@@ -0,0 +1,17 @@
+<?php
+// Welcome //
+
+
+      $servername = "putyourservername";
+ $username = "putyourusername";
+ $password = "putyourpassword";
+ $db = "putyourdb";
+
+
+
+ $conn = mysqli_connect($servername, $username, $password, $db) or  die("oops, something did not connect!");
+
+// anybody here the db you can use it this is my way  if you dont know how to connect the db or you keep forgetting \\
+
+//                                                        Thanks!                                                        \\
+?>
